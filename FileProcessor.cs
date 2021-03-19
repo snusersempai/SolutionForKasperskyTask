fb2b75668330f0091ac1c734e8dd69be81028392
Makar <80912223+snusersempai@users.noreply.github.com>
2021-03-19 14:44:05 +0300

    Comments added

--- FileProcessor.cs
+++ FileProcessor.cs
@@ -8,22 +8,22 @@ namespace solutionToKasperskyTask
     {
         static void Main(string[] args)
         {
-            FileProcessor fp = new FileProcessor(new HtmlHandler());
+            FileProcessor fp = new FileProcessor(new HtmlHandler());        // Declaring object of type class FileProcessor
             fp.ProcessFile(@"C:\Users\777\Desktop\test.html");              // Test for html file handling. Console should return "Html file handled".
 
-            fp.fileHandler = new TextHandler();
+            fp.fileHandler = new TextHandler();                             
             fp.ProcessFile(@"C:\Users\777\Desktop\test.txt");               // Test for text file handling. Console should return "Text file handled".
 
-            fp.fileHandler = new JsonHandler();
+            fp.fileHandler = new JsonHandler();                             
             fp.ProcessFile(@"C:\Users\777\Desktop\test.json");              // Test for JSON file handling. Console should return "JSON file handled".
 
-            fp.fileHandler = new JsonHandler();
+            fp.fileHandler = new JsonHandler();                                  
             fp.ProcessFile(@"C:\Users\777\Desktop\test.xml");               // Test for a file with unsupported extension input. Console should return 
                                                                             // "The extension you try to work with is not supported yet." and
                                                                             // "To work with " + TheFileExtension + " files check the Readme file"
 
             fp.fileHandler = new JsonHandler();                             
-            fp.ProcessFile(@"C:\Users\777\Desktop\test");               // Test for a file that does not exist. Console should return "File does not exist.".
+            fp.ProcessFile(@"C:\Users\777\Desktop\test");                   // Test for a file that does not exist. Console should return "File does not exist.".
             
             fp.fileHandler = new TextHandler();
             fp.ProcessFile(@"C:\Users\777\Desktop\test.json");              // Test for valid files sent to wrong handler. Console should return 
@@ -32,21 +32,21 @@ namespace solutionToKasperskyTask
     }
 
 
-    public abstract class AbstractFileHandler
+    public abstract class AbstractFileHandler                                                                               
     {
-        List<string> SupportedExtensions = new List<string>() {".html",".txt",".json"};
+        List<string> SupportedExtensions = new List<string>() {".html",".txt",".json"};                                     //List of supported extensions
         public string TheFileExtension;
 
         public void FileHandler(string fileName)
         {
-            if (File.Exists(fileName))
+            if (File.Exists(fileName))                                                                                      //Check does the file exist or not
             {
-                TheFileExtension = Path.GetExtension(fileName);
-                if (SupportedExtensions.Contains(TheFileExtension))
+                TheFileExtension = Path.GetExtension(fileName);                                                             //Get the extension of file
+                if (SupportedExtensions.Contains(TheFileExtension))                                                         
                 {
-                    if(TheFileExtension.Contains(ValidExtensionForMethod()))
+                    if(TheFileExtension.Contains(ValidExtensionForMethod()))                                                //Check if the file from handler suits its extension
                     {
-                        DoSomeStuff(fileName);
+                        DoSomeStuff(fileName);                                                                              //Calling function that overrided in specific handler
                     }
                     else
                     {
@@ -64,43 +64,53 @@ namespace solutionToKasperskyTask
                 Console.WriteLine("File does not exist.");
             }
         }
-        protected abstract void DoSomeStuff(string fileName);
+        protected abstract void DoSomeStuff(string fileName);                                                               //Declaring abstract function for work with file
 
-        protected abstract string ValidExtensionForMethod();
+        protected abstract string ValidExtensionForMethod();                                                                //Declaring abstract string for suitable extensions
+                                                                                                                            //from handlers
     }
 
-    public class FileProcessor
+    public class FileProcessor                                                                                              // FileProcessor class
     {
         public AbstractFileHandler fileHandler;
-        public FileProcessor(AbstractFileHandler fileHandler) => this.fileHandler = fileHandler;
+        public FileProcessor(AbstractFileHandler fileHandler) => this.fileHandler = fileHandler;                            //Qualifying the members of the class
         public void ProcessFile(string fileName)
         {
-            fileHandler.FileHandler(fileName);
+            fileHandler.FileHandler(fileName);                                                                              //Calling the file handler form abstract class
         }
     }
 
-    class HtmlHandler : AbstractFileHandler
+    class HtmlHandler : AbstractFileHandler                                                                                 // HtmlHandler class
     {
-        protected override string ValidExtensionForMethod() => ".html";
-        protected override void DoSomeStuff(string fileName)
+        protected override string ValidExtensionForMethod()
+        {
+            return ".html";                                                                                                 // Overriding the ValidExtensonForMethod()
+        }                                                     
+        protected override void DoSomeStuff(string fileName)                                                                // Some process with file
         {
             Console.WriteLine("Html file handled");
         }
     }
 
-    class TextHandler : AbstractFileHandler
+    class TextHandler : AbstractFileHandler                                                                                 // TextHandler class
     {
-        protected override string ValidExtensionForMethod() => ".txt";
-        protected override void DoSomeStuff(string fileName)
+        protected override string ValidExtensionForMethod()
+        {
+            return ".txt";                                                                                                  // Overriding the ValidExtensonForMethod()
+        }
+        protected override void DoSomeStuff(string fileName)                                                                // Some process with file
         {
             Console.WriteLine("Text file handled");
         }
     }
 
-    class JsonHandler : AbstractFileHandler
+    class JsonHandler : AbstractFileHandler                                                                                 // JsonHandler class
     {
-        protected override string ValidExtensionForMethod() => ".json";
-        protected override void DoSomeStuff(string fileName)
+        protected override string ValidExtensionForMethod()
+        {
+            return ".json";                                                                                                 // Overriding the ValidExtensonForMethod()
+        }
+        protected override void DoSomeStuff(string fileName)                                                                // Some process with file
         {
             Console.WriteLine("JSON file handled");
         }
